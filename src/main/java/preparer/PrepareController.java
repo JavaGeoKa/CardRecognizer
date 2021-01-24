77c27a1183785aa44d9ed51d7b9b88ea086aa2da
g <MDf4nBNjqrufGnxS@protonmail.com>
2021-01-24 16:39:55 +0300

    Color recognizer

--- src/main/java/preparer/PrepareController.java
+++ src/main/java/preparer/PrepareController.java
@@ -26,15 +26,15 @@ public class PrepareController {
     private static final String DIR_PATH_RED = "/home/g/IdeaProjects/CardRecognizer/cards/red";
     private static final String DIR_PATH_BLACK = "/home/g/IdeaProjects/CardRecognizer/cards/black";
 
-    //тест файл 5
+    //тест файл 5 {тк, ч, тч, к, чоб }  +
 //    private static final String PATH = "/home/g/Downloads/java_test_task/imgs/20180821_102328.773_0x1FE201D8.png";
-//    тест файл 4
+//    тест файл 4  {тч, ч, кроб, чоб} +
 //    private static final String PATH = "/home/g/Downloads/java_test_task/imgs/20180821_111814.694_0x1CFF023A.png";
-    //тест файл 3
+    //тест файл 3  {ч, ч, к}
     private static final String PATH = "/home/g/Downloads/java_test_task/imgs/20180821_091220.884_0x0EDA02B0.png";
 
 
-    static List<BufferedImage> currentCards = new ArrayList<>();
+    static Map<BufferedImage, String> currentCards = new HashMap<>();
 
 
     static Map<BufferedImage, String> redCards = new HashMap<>();
@@ -76,7 +76,7 @@ public class PrepareController {
         for (cardPlace = 0; cardPlace < 5; cardPlace++) {
              subImg = img.getSubimage(positions[cardPlace], y,29, 45);
             if (checkIsItCard(subImg)) {
-                currentCards.add(subImg);
+                currentCards.put(subImg, checkColor(subImg));
             }
         }
         cardPlace = 0;
@@ -84,14 +84,32 @@ public class PrepareController {
 
 
         //Здесь нужно начать сравнивать два изображения
+//        currentCards.forEach( i -> {
 
+
+//        });
 
 
 
 
     }
 
-//    || i.getRGB(l, 1) == -8882056
+
+
+    //определение цвета карты
+    private static String checkColor(BufferedImage i) {
+        if (i.getRGB(10,35) == -14474458) {
+            return "black";
+        } else if (i.getRGB(10,35) == -3323575) {
+            return "red";
+        }
+
+
+
+
+        return "red";
+    }
+
     //проверка карта или нет
     private static boolean checkIsItCard(BufferedImage i) {
         int backgroundQuantities = 0;
@@ -112,6 +130,10 @@ public class PrepareController {
             for (int l = 0; l < i.getWidth(); l++) {
                 if (i.getRGB(l, k) == -8882056) {
                     i.setRGB(l,k,-1);
+                } else if (i.getRGB(l,k) == -10477022) {
+                    i.setRGB(l, k, -3323575);
+                } else if (i.getRGB(l,k) == -15724526){
+                    i.setRGB(l, k, -14474458);
                 }
 
             }
